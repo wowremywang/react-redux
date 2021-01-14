5fa72c469a5f5016206c7a4140940b9340ea41af
remy727 <norioumata96727@gmail.com>
2021-01-14 03:27:23 -0500

    Add Redux middleware

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,8 +1,14 @@
 // src/js/store/index.js
 
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from '../reducers/index'
+import { forbiddenWordsMiddleware } from '../middleware'
 
-const store = createStore(rootReducer)
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware))
+)
 
 export default store
